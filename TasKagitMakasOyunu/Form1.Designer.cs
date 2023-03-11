f96db217ef83c7525c1940664ed3bbc221cc8636
HeronWenc <114104453+Heroinwenc@users.noreply.github.com>
2023-03-11 19:07:04 +0300

    Add files via upload

---
+++ TasKagitMakasOyunu/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace TasKagitMakasOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OyuncuSecimiLabel = new Guna.UI.WinForms.GunaLabel();
+            this.BilgisayarSecimiLabel = new Guna.UI.WinForms.GunaLabel();
+            this.SonucLabel = new Guna.UI.WinForms.GunaLabel();
+            this.OyuncuSkorLabel = new Guna.UI.WinForms.GunaLabel();
+            this.BilgisayarSkorLabel = new Guna.UI.WinForms.GunaLabel();
+            this.YenidenBaslatButton = new Guna.UI.WinForms.GunaButton();
+            this.OynaButton = new Guna.UI.WinForms.GunaAdvenceButton();
+            this.OyuncuSecimiComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // OyuncuSecimiLabel
+            // 
+            this.OyuncuSecimiLabel.AutoSize = true;
+            this.OyuncuSecimiLabel.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.OyuncuSecimiLabel.Location = new System.Drawing.Point(20, 363);
+            this.OyuncuSecimiLabel.Name = "OyuncuSecimiLabel";
+            this.OyuncuSecimiLabel.Size = new System.Drawing.Size(112, 15);
+            this.OyuncuSecimiLabel.TabIndex = 3;
+            this.OyuncuSecimiLabel.Text = "OyuncuSecimiLabel";
+            // 
+            // BilgisayarSecimiLabel
+            // 
+            this.BilgisayarSecimiLabel.AutoSize = true;
+            this.BilgisayarSecimiLabel.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.BilgisayarSecimiLabel.Location = new System.Drawing.Point(12, 387);
+            this.BilgisayarSecimiLabel.Name = "BilgisayarSecimiLabel";
+            this.BilgisayarSecimiLabel.Size = new System.Drawing.Size(120, 15);
+            this.BilgisayarSecimiLabel.TabIndex = 4;
+            this.BilgisayarSecimiLabel.Text = "BilgisayarSecimiLabel";
+            // 
+            // SonucLabel
+            // 
+            this.SonucLabel.AutoSize = true;
+            this.SonucLabel.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.SonucLabel.Location = new System.Drawing.Point(64, 288);
+            this.SonucLabel.Name = "SonucLabel";
+            this.SonucLabel.Size = new System.Drawing.Size(68, 15);
+            this.SonucLabel.TabIndex = 5;
+            this.SonucLabel.Text = "SonucLabel";
+            // 
+            // OyuncuSkorLabel
+            // 
+            this.OyuncuSkorLabel.AutoSize = true;
+            this.OyuncuSkorLabel.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.OyuncuSkorLabel.Location = new System.Drawing.Point(32, 312);
+            this.OyuncuSkorLabel.Name = "OyuncuSkorLabel";
+            this.OyuncuSkorLabel.Size = new System.Drawing.Size(100, 15);
+            this.OyuncuSkorLabel.TabIndex = 6;
+            this.OyuncuSkorLabel.Text = "OyuncuSkorLabel";
+            // 
+            // BilgisayarSkorLabel
+            // 
+            this.BilgisayarSkorLabel.AutoSize = true;
+            this.BilgisayarSkorLabel.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.BilgisayarSkorLabel.Location = new System.Drawing.Point(24, 339);
+            this.BilgisayarSkorLabel.Name = "BilgisayarSkorLabel";
+            this.BilgisayarSkorLabel.Size = new System.Drawing.Size(108, 15);
+            this.BilgisayarSkorLabel.TabIndex = 7;
+            this.BilgisayarSkorLabel.Text = "BilgisayarSkorLabel";
+            // 
+            // YenidenBaslatButton
+            // 
+            this.YenidenBaslatButton.AnimationHoverSpeed = 0.07F;
+            this.YenidenBaslatButton.AnimationSpeed = 0.03F;
+            this.YenidenBaslatButton.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.YenidenBaslatButton.BorderColor = System.Drawing.Color.Black;
+            this.YenidenBaslatButton.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.YenidenBaslatButton.FocusedColor = System.Drawing.Color.Empty;
+            this.YenidenBaslatButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.YenidenBaslatButton.ForeColor = System.Drawing.Color.White;
+            this.YenidenBaslatButton.Image = ((System.Drawing.Image)(resources.GetObject("YenidenBaslatButton.Image")));
+            this.YenidenBaslatButton.ImageSize = new System.Drawing.Size(20, 20);
+            this.YenidenBaslatButton.Location = new System.Drawing.Point(243, 197);
+            this.YenidenBaslatButton.Name = "YenidenBaslatButton";
+            this.YenidenBaslatButton.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.YenidenBaslatButton.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.YenidenBaslatButton.OnHoverForeColor = System.Drawing.Color.White;
+            this.YenidenBaslatButton.OnHoverImage = null;
+            this.YenidenBaslatButton.OnPressedColor = System.Drawing.Color.Black;
+            this.YenidenBaslatButton.Size = new System.Drawing.Size(160, 42);
+            this.YenidenBaslatButton.TabIndex = 8;
+            this.YenidenBaslatButton.Text = "YenidenBaslatButton";
+            this.YenidenBaslatButton.Click += new System.EventHandler(this.YenidenBaslatButton_Click);
+            // 
+            // OynaButton
+            // 
+            this.OynaButton.AnimationHoverSpeed = 0.07F;
+            this.OynaButton.AnimationSpeed = 0.03F;
+            this.OynaButton.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.OynaButton.BorderColor = System.Drawing.Color.Black;
+            this.OynaButton.CheckedBaseColor = System.Drawing.Color.Gray;
+            this.OynaButton.CheckedBorderColor = System.Drawing.Color.Black;
+            this.OynaButton.CheckedForeColor = System.Drawing.Color.White;
+            this.OynaButton.CheckedImage = ((System.Drawing.Image)(resources.GetObject("OynaButton.CheckedImage")));
+            this.OynaButton.CheckedLineColor = System.Drawing.Color.DimGray;
+            this.OynaButton.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.OynaButton.FocusedColor = System.Drawing.Color.Empty;
+            this.OynaButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.OynaButton.ForeColor = System.Drawing.Color.White;
+            this.OynaButton.Image = ((System.Drawing.Image)(resources.GetObject("OynaButton.Image")));
+            this.OynaButton.ImageSize = new System.Drawing.Size(20, 20);
+            this.OynaButton.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(58)))), ((int)(((byte)(170)))));
+            this.OynaButton.Location = new System.Drawing.Point(243, 149);
+            this.OynaButton.Name = "OynaButton";
+            this.OynaButton.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.OynaButton.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.OynaButton.OnHoverForeColor = System.Drawing.Color.White;
+            this.OynaButton.OnHoverImage = null;
+            this.OynaButton.OnHoverLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(58)))), ((int)(((byte)(170)))));
+            this.OynaButton.OnPressedColor = System.Drawing.Color.Black;
+            this.OynaButton.Size = new System.Drawing.Size(160, 42);
+            this.OynaButton.TabIndex = 9;
+            this.OynaButton.Text = "OynaButton";
+            this.OynaButton.Click += new System.EventHandler(this.OynaButton_Click);
+            // 
+            // OyuncuSecimiComboBox
+            // 
+            this.OyuncuSecimiComboBox.FormattingEnabled = true;
+            this.OyuncuSecimiComboBox.Items.AddRange(new object[] {
+            "Taş",
+            "Kağıt",
+            "Makas"});
+            this.OyuncuSecimiComboBox.Location = new System.Drawing.Point(97, 71);
+            this.OyuncuSecimiComboBox.Name = "OyuncuSecimiComboBox";
+            this.OyuncuSecimiComboBox.Size = new System.Drawing.Size(121, 21);
+            this.OyuncuSecimiComboBox.TabIndex = 10;
+            this.OyuncuSecimiComboBox.SelectedIndexChanged += new System.EventHandler(this.OyuncuSecimiComboBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 411);
+            this.Controls.Add(this.OyuncuSecimiComboBox);
+            this.Controls.Add(this.OynaButton);
+            this.Controls.Add(this.YenidenBaslatButton);
+            this.Controls.Add(this.BilgisayarSkorLabel);
+            this.Controls.Add(this.OyuncuSkorLabel);
+            this.Controls.Add(this.SonucLabel);
+            this.Controls.Add(this.BilgisayarSecimiLabel);
+            this.Controls.Add(this.OyuncuSecimiLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaLabel OyuncuSecimiLabel;
+        private Guna.UI.WinForms.GunaLabel BilgisayarSecimiLabel;
+        private Guna.UI.WinForms.GunaLabel SonucLabel;
+        private Guna.UI.WinForms.GunaLabel OyuncuSkorLabel;
+        private Guna.UI.WinForms.GunaLabel BilgisayarSkorLabel;
+        private Guna.UI.WinForms.GunaButton YenidenBaslatButton;
+        private Guna.UI.WinForms.GunaAdvenceButton OynaButton;
+        private System.Windows.Forms.ComboBox OyuncuSecimiComboBox;
+
+
+    }
+}
+
---
+++ TasKagitMakasOyunu/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TasKagitMakasOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        Random random = new Random();
+        int oyuncuSkor = 0;
+        int bilgisayarSkor = 0;
+        TkmSecimleri oyuncuSecimi;
+
+        enum TkmSecimleri
+        {
+            Tas,
+            Kagit,
+            Makas
+        }
+
+
+        private void OyuncuSecimiComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string secim = OyuncuSecimiComboBox.SelectedItem.ToString();
+            switch (secim)
+            {
+                case "Taş":
+                    oyuncuSecimi = TkmSecimleri.Tas;
+                    break;
+                case "Kağıt":
+                    oyuncuSecimi = TkmSecimleri.Kagit;
+                    break;
+                case "Makas":
+                    oyuncuSecimi = TkmSecimleri.Makas;
+                    break;
+                default:
+                    oyuncuSecimi = TkmSecimleri.Tas;
+                    break;
+            }
+        }
+
+        private TkmSecimleri BilgisayarSecimi()
+        {
+            TkmSecimleri[] secimler = { TkmSecimleri.Tas, TkmSecimleri.Kagit, TkmSecimleri.Makas };
+            int index = random.Next(secimler.Length);
+            return secimler[index];
+        }
+
+        private void OynaButton_Click(object sender, EventArgs e)
+        {
+            TkmSecimleri bilgisayarSecimi = BilgisayarSecimi();
+
+            string oyuncuSecimiMetni = oyuncuSecimi.ToString();
+            string bilgisayarSecimiMetni = bilgisayarSecimi.ToString();
+
+            OyuncuSecimiLabel.Text = "Oyuncunun Seçimi: " + oyuncuSecimiMetni;
+            BilgisayarSecimiLabel.Text = "Bilgisayarın Seçimi: " + bilgisayarSecimiMetni;
+
+            if (oyuncuSecimi == bilgisayarSecimi)
+            {
+                SonucLabel.Text = "Berabere!";
+            }
+            else if ((oyuncuSecimi == TkmSecimleri.Tas && bilgisayarSecimi == TkmSecimleri.Makas) ||
+                (oyuncuSecimi == TkmSecimleri.Kagit && bilgisayarSecimi == TkmSecimleri.Tas) ||
+                (oyuncuSecimi == TkmSecimleri.Makas && bilgisayarSecimi == TkmSecimleri.Kagit))
+            {
+                oyuncuSkor++;
+                OyuncuSkorLabel.Text = "Oyuncu: " + oyuncuSkor.ToString();
+                SonucLabel.Text = "Oyuncu kazandı!";
+            }
+            else
+            {
+                bilgisayarSkor++;
+                BilgisayarSkorLabel.Text = "Bilgisayar: " + bilgisayarSkor.ToString();
+                SonucLabel.Text = "Bilgisayar kazandı!";
+            }
+        }
+
+        private void YenidenBaslatButton_Click(object sender, EventArgs e)
+        {
+            oyuncuSkor = 0;
+            bilgisayarSkor = 0;
+            OyuncuSkorLabel.Text = "Oyuncu: 0";
+            BilgisayarSkorLabel.Text = "Bilgisayar: 0";
+            OyuncuSecimiLabel.Text = "Oyuncunun Seçimi: ";
+            BilgisayarSecimiLabel.Text = "Bilgisayarın Seçimi: ";
+            SonucLabel.Text = "";
+            OyuncuSecimiComboBox.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OyuncuSecimiComboBox.SelectedIndex = 0;
+        }
+    }
+}
